85844c1db189ff6c7a5d43fdb1d524adae7a2c19
Dan Taylor <dan@logicalgenetics.com>
2019-03-26 18:50:31 +0000

    Added exploded date fields for easy grouping

--- data_preparation.py
+++ data_preparation.py
@@ -1,6 +1,7 @@
 import calendar
 import json
 import os
+import shutil
 import time
 import traceback
 from datetime import datetime
@@ -53,59 +54,6 @@ def download_data_files(remote_files, cache_dir):
         yield filename
 
 
-def date_str_to_datetime(date_str):
-    try:
-        return datetime.strptime(date_str[:16], '%d/%m/%Y %H:%M')
-    except (ValueError, TypeError):
-        return None
-
-
-def datetime_to_unix_ts(dt):
-    return int(dt.strftime("%s"))
-
-
-def datetime_to_weekday(dt):
-    return calendar.day_name[dt.weekday()]
-
-
-def explode_date(df, date_field, prefix):
-    df[prefix + '_dt'] = df[date_field].apply(date_str_to_datetime)
-    df = df.dropna()
-
-    split_df = df[date_field].str.split("[/ :]", expand=True)
-    df[prefix + '_day'] = split_df[0]
-    df[prefix + '_month'] = split_df[1]
-    df[prefix + '_year'] = split_df[2]
-    df[prefix + '_hour'] = split_df[3]
-    df[prefix + '_day_of_week'] = df[prefix + '_dt'].apply(datetime_to_weekday)
-    df[prefix + '_ts'] = df[prefix + '_dt'].apply(datetime_to_unix_ts)
-    df.drop([date_field, prefix + '_dt'], axis=1)
-    return df
-
-
-def load_data_old(data_files):
-    files = [f for f in data_files]
-    dfs = []
-
-    for f in files:
-        print("Loading: " + f)
-        raw = pd.read_csv(f, index_col=None, header=0)
-
-        try:
-            print(raw.columns)
-            df = raw[['Bike Id', 'Duration', 'StartStation Id', 'EndStation Id', 'Start Date', 'End Date']]
-            df = df.dropna()
-            df.columns = ['bike_id', 'duration', 'start_station_id', 'end_station_id', 'start_str', 'end_str']
-            df = explode_date(df, 'start_str', 'start')
-            df = explode_date(df, 'end_str', 'end')
-            dfs.append(df)
-        except Exception:
-            print(traceback.format_exc())
-            continue
-
-    return pd.concat(dfs, axis=0, ignore_index=True)
-
-
 def load_data(data_dir):
     schema = T.StructType([
         T.StructField("Rental Id",          T.IntegerType(),    False),
@@ -120,6 +68,7 @@ def load_data(data_dir):
     ])
 
     df = spark.read.csv(
+        #path="{0}/1a.JourneyDataExtract04Jan15-17Jan15.csv".format(data_dir),
         path="{0}/*.csv".format(data_dir),
         header=True,
         schema=schema,
@@ -136,6 +85,26 @@ def load_data(data_dir):
         .withColumnRenamed("StartStation Id", "start_station_id")\
         .withColumnRenamed("StartStation Name", "start_station_name")
 
+    df = explode_date_str(df, "start_date", "start_")
+    df = explode_date_str(df, "end_date", "end_")
+
+    return df
+
+
+def explode_date_str(df, input_field, prefix):
+    ts_field = prefix + 'ts'
+
+    df = df.withColumn(
+        ts_field,
+        F.to_timestamp(input_field, 'dd/MM/yyyy HH:mm')
+    )
+    df = df.drop(input_field)
+    df = df\
+        .withColumn(prefix + 'year', F.year(ts_field)) \
+        .withColumn(prefix + 'month', F.month(ts_field)) \
+        .withColumn(prefix + 'day', F.dayofmonth(ts_field)) \
+        .withColumn(prefix + 'day_of_week', F.dayofweek(ts_field)) \
+        .withColumn(prefix + 'hour', F.hour(ts_field))
     return df
 
 
@@ -152,22 +121,21 @@ def filter_data(trip_data):
 def main():
     bike_points_url = "https://api.tfl.gov.uk/bikepoint"
     index_url = "https://cycling.data.tfl.gov.uk/usage-stats/cycling-load.json"
-    output_dir = "data/parquet"
+    output_dir = "data/parquet_trip"
     raw_trip_dir = "data/raw_trip"
 
     if not os.path.exists(raw_trip_dir):
         os.makedirs(raw_trip_dir)
 
     if os.path.exists(output_dir):
-        os.removedirs(output_dir)
+        shutil.rmtree(output_dir)
 
-    bike_stations = download_bike_stations(bike_points_url)
+    #bike_stations = download_bike_stations(bike_points_url)
+    #print("Loaded bike stations:")
+    #print(bike_stations)
 
-    print("Loaded bike stations:")
-    print(bike_stations)
-
-    remote_files = get_remote_files_from_index(index_url)
-    download_data_files(remote_files, raw_trip_dir)
+    #remote_files = get_remote_files_from_index(index_url)
+    #download_data_files(remote_files, raw_trip_dir)
 
     trip_data = load_data(raw_trip_dir)
     trip_data = filter_data(trip_data)
@@ -191,6 +159,5 @@ if __name__ == "__main__":
 
     start = time.time()
     main()
-
     end = time.time()
     print("Data prep took {0:06.2f}s".format(end - start))
